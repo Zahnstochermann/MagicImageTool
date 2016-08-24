006f22ed11cbae074c7438905a639ad206ef4f43
Zahnstochermann <jan.deters@gmx.de>
2016-08-24 18:44:21 +0200

    rename to resize, some ideas in comments

--- resize.sh
+++ resize.sh
@@ -2,16 +2,21 @@
 # this script uses ImageMagic https://imagemagick.org/
 # install on mac: brew install imagemagick
 
+# read -p "Enter the maximum width: " -s width
+# echo
+# read -p "Enter the maximum height: " -s height
+# echo
 read -p "Warning: Files will be overwritten! Continue? (y/n) " -n 1 -r
 echo    # (optional) move to a new line
+
 if [[ $REPLY =~ ^[Yy]$ ]]
   then
-      # do dangerous stuff
-      for file in *.png
-        do
-          convert $file -resize 150x150 $file;
-          echo "converting $file"
-      done
+    for file in *.png
+      do
+        # convert $file -resize $width x $height $file;
+        convert $file -resize 150x150 $file;
+        echo "converting $file"
+    done
   else
     echo "Abort by user"
 fi
