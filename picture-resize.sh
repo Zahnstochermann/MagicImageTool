b89687bd9c717594d80328270d1e205b15680626
Zahnstochermann <jan.deters@gmx.de>
2016-08-24 18:30:16 +0200

    add confirmation

--- picture-resize.sh
+++ picture-resize.sh
@@ -2,8 +2,16 @@
 # this script uses ImageMagic https://imagemagick.org/
 # install on mac: brew install imagemagick
 
-for file in *.png
-  do
-    convert $file -resize 150x150 150-$file;
-    echo "converting $file"
-done
+read -p "Warning: Files will be overwritten! Continue? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+      # do dangerous stuff
+      for file in *.png
+        do
+          convert $file -resize 150x150 $file;
+          echo "converting $file"
+      done
+  else
+    echo "Abort by user"
+fi
